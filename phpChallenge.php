e2cd7e7f8aa39fde5d4a9227a8f531999e184103
Don-Stevenson <dsteven1@gmail.com>
2021-05-07 11:41:59 -0400

    solve problem to spec, checks if guess is the num

--- phpChallenge.php
+++ phpChallenge.php
@@ -1,8 +1,4 @@
 <?php
-$min = 1;
-$max = 50;
-$guess = 4; // change this value to test it
-$num = 16; // change this value to test it
 
 // $num = rand($min, $max) <= implement this
 // write a function that checks the guess to see if it matches the num.
@@ -11,9 +7,47 @@ $num = 16; // change this value to test it
 // try it with the random num generator to see if it is really working
 
 function testGuess($myGuess)
-{ 
-   echo $myGuess;
+{
+   $min = 1;
+   $max = 50;
+   $guess = 4; // change this value to test it
+   $num = 16; // change this value to test it
+   
+   // prints the guess that has been entered
+    echo $myGuess;
+    
+    // checks if the number is actually a number
+    if (!is_numeric($myGuess)) {
+        echo "<p>". $myGuess ." is not a number </p>";
+        
+        // check if myguess is within range 
+    } else {
+        if ($myGuess > $max || $myGuess < $min) {
+            echo "<p>". $myGuess ." is not within the range specified </p>";
+            
+            //  is myguess the number? if not, print this message  
+        } elseif ($myGuess != $num) {
+            echo "<p>". $myGuess ." is not the correct guess </p>";
+         
+           // only when myguess is the number print the following  
+         } else {
+            echo "<p>". $myGuess ." is the correct guess!! Awesome work! </p>";
+        }
+    }
 }
 
 
-testGuess(13);
+// testGuess("hello");
+// expect "<p>". $myGuess ." is not a number </p>";
+
+// testGuess(100);
+//  expect "<p>". $myGuess ." is not with the range specified </p>";
+
+// testGuess(-1);
+//  expect "<p>". $myGuess ." is not with the range specified </p>";
+
+// testGuess(5);
+//  expect "<p>". $myGuess ." is not the correct guess </p>";
+
+testGuess(16);
+//  expect "<p>". $myGuess ." is the correct guess!! Awesome work! </p>";
