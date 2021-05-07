83fcb524e023ce742533cbab32932cc2946a83cf
Don-Stevenson <dsteven1@gmail.com>
2021-05-07 09:50:54 -0400

    add challenge setup

---
+++ phpChallenge.php
@@ -0,0 +1,20 @@
+<?php
+$min = 1;
+$max = 50;
+$guess = 4; // change this value to test it
+$num = 16; // change this value to test it
+
+// $num = rand($min, $max) <= implement this
+// write a function that checks the guess to see if it matches the num.
+// The guess has to be within the range between min an max
+// use if else else if blocks to solve
+// try it with the random num generator to see if it is really working
+
+
+function testGuess($guess)
+{
+    echo $guess;
+}
+
+
+testGuess(13);
