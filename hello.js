b25dc26e30b22542806a301243ed9cff686c0299
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-08-30 20:26:35 -0400

    Swap console.logs for return statements

--- hello.js
+++ hello.js
@@ -1,9 +1,11 @@
 const helloWorld = (dayOfTheWeek) => {
   if (dayOfTheWeek === "Thursday") {
-    console.log("Hi There");
+    return "Hi There"
   } else if (dayOfTheWeek === "Wednesday") {
-    console.log("It's Hump Day");
+    return "It's Hump Day"
   } else {
-    console.log("It's not Thursday");
+    return "It's not Thursday"
   }
 };
+
+console.log(helloWorld("Thursday"))
